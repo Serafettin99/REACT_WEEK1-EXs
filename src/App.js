b4390a4bb7798bfe58699412ee1d9827fa6dd392
Serafettin Bayram <sbayram2016@gmail.com>
2020-01-18 23:10:30 +0100

    week1 exercises 1, 2, 3 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//Components
+import HobbyList from './components/exercise-1/HobbyList';
+import Guarantee from './components/exercise-2/Guarantee';
+import Counter from './components/exercise-3/Counter';
+//Images
+import coin from './components/pics/coin.png';
+import chat from './components/pics/chat.png';
+import delivery from './components/pics/f-delivery.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HobbyList className="hobby-list" />
+
+      <br />
+      <div className="guarantee">
+        <Guarantee
+          img={delivery}
+          title="Free shipping"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+          alt="truck"
+        />
+        <Guarantee
+          img={coin}
+          title="100% Money back"
+          description="Phasellus nunc lorem, dignissim vitae lobortis sit amet, eleifend non sapien"
+          alt="geld"
+        />
+        <Guarantee
+          img={chat}
+          title="Online support 24/7"
+          description="Mauris posuere aliquet mauris, in tincidunt ipsum"
+          alt="chat"
+        />
+      </div>
+      <br />
+      <Counter />
     </div>
   );
 }
